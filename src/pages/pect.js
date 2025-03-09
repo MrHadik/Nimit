61306c4b393a98ebb421bed2f20d0b4717f78392
MrHadik <chavdah80@gmail.com>
2025-03-09 14:00:07 +0530

    code update

--- src/pages/pect.js
+++ src/pages/pect.js
@@ -68,7 +68,7 @@ function Pect() {
 
         const responseData = await response.json();
         if (responseData.success) {
-          const response = await fetch(`https://hook.eu2.make.com/pv28ymbt6ph9l5kw7z45ugdk3qxrbd3m?title=${record.message}&startDate=${record.date.toLocaleDateString()}&endDate=${record.date.toLocaleDateString()}`, {
+          const response = await fetch(`https://hook.eu2.make.com/pv28ymbt6ph9l5kw7z45ugdk3qxrbd3m?title=${record.message}&startDate=${record.date.toLocaleDateString("en-US")}&endDate=${record.date.toLocaleDateString("en-US")}`, {
             method: 'GET',
           });
           if(!response.ok) {
